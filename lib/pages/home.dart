4c8729a81b0f8a8f8db708bbeb766f1453ab77ae
David Marro <davidmarro@MBP-di-David.homenet.telecomitalia.it>
2020-03-22 18:21:25 +0100

    expandble modal prova

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,9 @@
+
+
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 import 'package:flutter/material.dart';
-
+import 'package:expandable/expandable.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -10,49 +12,120 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+
+  Widget buildBottomSheet() {
+      return Container(
+          color: Colors.white,
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            RadioListTile(dense: true, title: Text('Test'), groupValue: 'test', onChanged: (value) {}, value: true),
+            RadioListTile(dense: true, title: Text('Test'), groupValue: 'test', onChanged: (value) {}, value: true),
+          ]));
+    }
+
+
   @override
   int currentPage=0;
   Widget build(BuildContext context) {
    
   return Scaffold(
-      bottomNavigationBar: FancyBottomNavigation(
-        tabs: [
-        TabData(iconData: Icons.home, title: "Home"),
-        //TabData(iconData: Icons.search, title: "Search"),
-        TabData(iconData: Icons.subtitles, title: "Percorso")
-        ],
-        onTabChangedListener: (position) {
-        setState(() {
-         currentPage = position;
-        });
+
+      bottomNavigationBar:  ExpandableNotifier( child: 
+      Column(mainAxisSize: MainAxisSize.min,
+        children: <Widget>[Expandable(expanded: buildBottomSheet(),),ExpandableButton(
+                child: SizedBox(height: 50,
+                  child: Center(
+                    child: Icon(Icons.edit),
+                  ),
+                ),
+              ),
+          FancyBottomNavigation(
+            
+            tabs: [
+            TabData(iconData: Icons.home, title: "Home",),
+            //TabData(iconData: Icons.search, title: "Search"),
+            TabData(iconData: Icons.subtitles, title: "Percorso",)
+            ],
+            onTabChangedListener: (position) {
+            setState(() {
+             currentPage = position;
+            });
 
     },
-        inactiveIconColor: Colors.orange,
-        activeIconColor: Colors.white,
-        circleColor: Colors.orange,
-      ),
+            inactiveIconColor: Colors.grey,
+            activeIconColor: Colors.grey,
+            circleColor: Colors.white,
 
-      body: Placeholder(fallbackHeight: MediaQuery.of(context).size.height, fallbackWidth: MediaQuery.of(context).size.width),
+          ),
+        ],
+      ),),
+
+//imagebackground
+      body: Column(
+        children: <Widget>[
+          Column(children: <Widget>[
+            
+            Stack(children: <Widget>[
+              Placeholder(fallbackHeight: MediaQuery.of(context).size.height * 0.75, fallbackWidth: MediaQuery.of(context).size.height ),
+              Positioned(left: 10,right: 10,
+                child: Container(
+              margin: EdgeInsets.only(top:20),
+              child: FittedBox(
+                child: Container(
+                  width: 65,
+                  height: 65,
+                  margin: const EdgeInsets.only(bottom: 32),
+                  child: RaisedButton(
+                  onPressed: () {
+                  // Add your onPressed code here!
+                  },
+        child: Icon(Icons.camera,size:30,),
+      //  backgroundColor: Colors.orange,
+        elevation: 3,
+        
+        ),
+      ),),),),],
+                           
+            ),
+            
+            Container(
+              width: 500,
+              height: MediaQuery.of(context).size.height * 0.16,
+              child: DecoratedBox(decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Colors.white10),
+                       
+            ),
+            
+              ),
+          ],
+         ),
+        ],
+      )
+          ,
+          
           backgroundColor: Colors.white,
-          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+         // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
           floatingActionButton: Container(
-            width: 65,
-            height: 65,
+            margin: EdgeInsets.only(top:100),
             child: FittedBox(
-                          child: FloatingActionButton(
-      
-        onPressed: () {
-        // Add your onPressed code here!
-        },
-
-        child: Icon(Icons.camera,size: 26,),
+              child: Container(
+                width: 65,
+                height: 65,
+                margin: const EdgeInsets.only(bottom: 32),
+                child: FloatingActionButton(
+                onPressed: () {
+                // Add your onPressed code here!
+                },
+        child: Icon(Icons.camera,size:30,),
         backgroundColor: Colors.orange,
         elevation: 3,
         
+        ),
       ),
-            ),
-          ),
-    );
+    ),
+  ),      
+);
         
       
 
